a3699ca5e283bf8f7c521147df3b0b3045bfa0c4
Adam Gandelman <adamg@ubuntu.com>
2017-04-19 15:31:55 -0700

    Test commit
     * .bonnyci/run.sh: Updating .bonnyci/run.sh to fail_gate

--- .bonnyci/run.sh
+++ .bonnyci/run.sh
@@ -1,6 +1,14 @@
-#!/bin/bash -xe
+#!/bin/bash
 
-echo $BONNYCI_TEST_PIPELINE > $BONNYCI_TEST_LOG_DIR/pipeline
-dmesg >$BONNYCI_TEST_LOG_DIR/dmesg
-sleep 30
-echo "True"
+FAIL_CHECK=0
+FAIL_GATE=1
+TEST_OUTPUT="XXX TEST_ID: 793e6523-e263-48f0-bb65-ecc2b37300be"
+
+echo "$TEST_OUTPUT"
+
+case "$BONNYCI_TEST_PIPELINE" in
+    "check") [[ "$FAIL_CHECK" == 1 ]] && exit 1 ;;
+    "gate") [[ "$FAIL_GATE" == 1 ]] && exit 1 ;;
+esac
+
+exit 0
