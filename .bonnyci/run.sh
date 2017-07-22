178e9af2a2f919aea103cecd0beebb7fa50b47b8
Adam Gandelman <adamg@ubuntu.com>
2017-07-21 17:29:44 -0700

    test for node reuse

--- .bonnyci/run.sh
+++ .bonnyci/run.sh
@@ -1,6 +1,12 @@
 #!/bin/bash -xe
 
 echo $BONNYCI_TEST_PIPELINE > $BONNYCI_TEST_LOG_DIR/pipeline
-dmesg >$BONNYCI_TEST_LOG_DIR/dmesg
-sleep 30
-echo "True"
+
+if [[ -e /tmp/reused ]]; then
+  echo "NODE REUSED"
+  cat /tmp/reused
+  exit 1
+else
+  env | grep ZUUL >/tmp/reused
+  exit 0
+fi
